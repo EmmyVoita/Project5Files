b3d1056754d572ef38a96933fe02045f92d81b5f
Emmy V <82542924+sweetpotatoAster@users.noreply.github.com>
2023-03-06 00:01:47 -0700

    Update SSAOPassClass.h

--- HeaderFiles/SSAOPassClass.h
+++ HeaderFiles/SSAOPassClass.h
@@ -20,18 +20,65 @@ class SSAOPassClass
 		unsigned int ambientOcclusionFrameBuffer;
 		unsigned int ambientOcclusionTexture;
 		unsigned int depthRenderBuffer;
+		unsigned int depthMapFBO;
+		unsigned int depthTexture;
 	
 
-		const unsigned int TEX_WIDTH = 1024, TEX_HEIGHT = 1024;
+		const unsigned int TEX_WIDTH = 1400, TEX_HEIGHT = 800;
 
 		//Shader reference
 		Shader2* simpleSSAOShader;
+		Shader2* renderDepthShader;
 
 		//Models reference used for rendering
 		vector<MyModelClass*> myModels;
 
 		glm::mat4 ViewMatrix;
 
+		void initDepthMap()
+		{
+			// Create a framebuffer object for rendering the depth map
+			glGenTextures(1, &depthTexture);
+			glBindTexture(GL_TEXTURE_2D, depthTexture);
+			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, TEX_WIDTH, TEX_HEIGHT, 0, GL_RGBA, GL_UNSIGNED_BYTE, NULL);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+			glGenFramebuffers(1, &depthMapFBO);
+			glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+			glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, depthTexture, 0);
+			glDrawBuffer(GL_COLOR_ATTACHMENT0);
+			glReadBuffer(GL_COLOR_ATTACHMENT0);
+
+			// check for errors
+			GLenum status = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+			if (status != GL_FRAMEBUFFER_COMPLETE) {
+				printf("Error: Shadow map framebuffer is not complete!\n");
+				exit(1);
+			}
+
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		}
+
+
+		void renderDepthMap()
+		{
+			// render scene from camera
+			renderDepthShader->Use();
+
+
+			glViewport(0, 0, TEX_WIDTH, TEX_HEIGHT);
+			glBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
+				glClear(GL_DEPTH_BUFFER_BIT);
+				//render the scene for the depth texture
+				renderModels();
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+			
+			// reset viewport
+			glViewport(0, 0, 1400, 800);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		}
 
 		void initSSAOTextureAndRBO()
 		{
@@ -43,6 +90,8 @@ class SSAOPassClass
 			glGenTextures(1, &ambientOcclusionTexture);
 			glBindTexture(GL_TEXTURE_2D, ambientOcclusionTexture);
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RED, TEX_WIDTH, TEX_HEIGHT, 0, GL_RED, GL_FLOAT, NULL);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 
@@ -60,13 +109,13 @@ class SSAOPassClass
 			// this allows the SSAO fragment shader to access the depth information stored in the depth render buffer.
 
 			// Generate depth render buffer
-			glGenRenderbuffers(1, &depthRenderBuffer);
-			glBindRenderbuffer(GL_RENDERBUFFER, depthRenderBuffer);
-			glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT,  TEX_WIDTH, TEX_HEIGHT);
-			glBindRenderbuffer(GL_RENDERBUFFER, 0);
+			//glGenRenderbuffers(1, &depthRenderBuffer);
+			//glBindRenderbuffer(GL_RENDERBUFFER, depthRenderBuffer);
+			//glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT,  TEX_WIDTH, TEX_HEIGHT);
+			//glBindRenderbuffer(GL_RENDERBUFFER, 0);
 
 			// attach depth render buffer to framebuffer
-    		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderBuffer);
+    		//glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, depthRenderBuffer);
 
 
 			 // check for framebuffer completeness
@@ -79,11 +128,11 @@ class SSAOPassClass
 
 		}
 
-
 		void renderScene()
 		{
 			// To render to an FBO texture, you need to bind the FBO, then render the objects.
 			simpleSSAOShader->Use();
+			simpleSSAOShader->setMat4fv(ViewMatrix, "ViewMatrix");
 
 			glViewport(0, 0, TEX_WIDTH, TEX_HEIGHT);												// Set the dimensions of the viewport to render to the texture
 			glBindFramebuffer(GL_FRAMEBUFFER, ambientOcclusionFrameBuffer);							// Bind SSAO FBO for rendering
@@ -99,9 +148,9 @@ class SSAOPassClass
 		void renderModels()
 		{
 			//Render the scene just for depth to be used for SSAO.
-			for (int i = 0; i < myModels.size(); i++)
+			for(int i = 0; i < myModels.size(); i++)
 			{
-				myModels[i]->renderForAmbientOcclusion(*simpleSSAOShader);
+				myModels[i]->renderForAmbientOcclusion(*renderDepthShader);
 			}
 		}
 
@@ -115,18 +164,26 @@ class SSAOPassClass
 			this->ViewMatrix = ViewMatrix;
 			//Create a new shader for SSAO
 			simpleSSAOShader = new Shader2("Shaders/simpleSSAOShader.vs", "Shaders/simpleSSAOShader.frag");
+			renderDepthShader = new Shader2("Shaders/renderForDepth.vs", "Shaders/renderForDepth.frag");
 			
-			initSSAOTextureAndRBO();
+			initDepthMap();
+			//initSSAOTextureAndRBO();
 		}
 
 		unsigned int* getDepthMap()
 		{
-			return &ambientOcclusionTexture;
+			return &depthTexture;
+		}
+
+		unsigned int getDepthMap2()
+		{
+			return depthTexture;
 		}
 
 		void render()
 		{
-			renderScene();
+			renderDepthMap();
+			//renderScene();
 		}
 
 };
